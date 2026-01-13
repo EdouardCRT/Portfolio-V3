80a3560a99acb8a47df466bac240861888efb05e
Edouard Courty <edouard.courty@epitech.eu>
2026-01-13 18:38:01 +0100

    changes for base in config possible solve for 404 error in deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Portfolio-V3/',
+  base: process.env.NODE_ENV === 'production' ? '/Portfolio-V3/' : '/',
   server: {
     port: 3001,
   },
